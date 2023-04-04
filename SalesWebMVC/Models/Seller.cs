f76fdcfe5f069ab3a7a5d2e4a4254e7e9f12cbb4
Cezar Camargo da Silva <fe652@t-systems.com.br>
2023-04-04 17:52:07 -0300

    Foreign key not null

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -6,12 +6,14 @@ namespace SalesWebMVC.Models
 {
     public class Seller
     {
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
