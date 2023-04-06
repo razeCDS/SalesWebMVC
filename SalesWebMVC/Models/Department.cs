a2cf5698d860e5004e98107b83feec4a328819e8
Cezar Camargo da Silva <fe652@t-systems.com.br>
2023-04-06 19:35:37 -0300

    Implementing grouping search

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -25,7 +25,7 @@ namespace SalesWebMVC.Models
             Sellers.Add(seller);
         }
 
-        public double Totalsales(DateTime initial, DateTime final)
+        public double TotalSales(DateTime initial, DateTime final)
         {
             return Sellers.Sum(seller => seller.TotalSales(initial, final));
             // pra cada seller na lista, some os valores de venda total entre a data inicial e a data final.
